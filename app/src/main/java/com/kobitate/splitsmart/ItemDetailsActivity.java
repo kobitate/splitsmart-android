da2f1e7da0206fa7e36debf54f115949987550c5
Kobi Tate <kobi@kobitate.com>
2017-08-24 22:24:59 -0400

    Added per pound functionality

--- app/src/main/java/com/kobitate/splitsmart/ItemDetailsActivity.java
+++ app/src/main/java/com/kobitate/splitsmart/ItemDetailsActivity.java
@@ -9,16 +9,25 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
 
 public class ItemDetailsActivity extends AppCompatActivity {
 
@@ -28,6 +37,14 @@ public class ItemDetailsActivity extends AppCompatActivity {
 	ImageView thumb;
 
 	FloatingActionButton submitFAB;
+
+	CheckBox isWeightBox;
+	LinearLayout weightLayout;
+	TextView weightValue;
+	TextView finalPriceView;
+	LinearLayout weightPriceSummary;
+
+	double byWeightPrice = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +67,12 @@ public class ItemDetailsActivity extends AppCompatActivity {
 
 		submitFAB = (FloatingActionButton) findViewById(R.id.details_submit);
 
+		isWeightBox = (CheckBox) findViewById(R.id.perPoundCheck);
+		weightLayout = (LinearLayout) findViewById(R.id.weightEntry);
+		weightValue = (TextView) findViewById(R.id.weightValue);
+		finalPriceView = (TextView) findViewById(R.id.weightPrice);
+		weightPriceSummary = (LinearLayout) findViewById(R.id.poundPriceSummary);
+
 		if (getIntent().hasExtra("scanned_item_name") && getIntent().hasExtra("scanned_item_price")) {
 			inputName.setText(getIntent().getStringExtra("scanned_item_name"));
 			inputPrice.setText(String.valueOf(getIntent().getDoubleExtra("scanned_item_price",0)));
@@ -61,17 +84,42 @@ public class ItemDetailsActivity extends AppCompatActivity {
 			thumb.setMinimumWidth(100);
 		}
 
+		TextWatcher weightWatchers = new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+				if (isWeightBox.isChecked()) {
+					updateByWeightPrice();
+				}
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+
+			}
+		};
+
+		weightValue.addTextChangedListener(weightWatchers);
+		inputPrice.addTextChangedListener(weightWatchers);
 
 		submitFAB.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				if (inputName.getText().length() == 0 || inputPrice.getText().length() == 0) {
+				if (inputName.getText().length() == 0 || inputPrice.getText().length() == 0 || (isWeightBox.isChecked() && weightValue.getText().length() == 0)) {
 					Toast.makeText(ItemDetailsActivity.this, "Please enter the item name and price to continue.", Toast.LENGTH_SHORT).show();
 				}
 				else {
 					AppDB db = AppDB.getInstance(view.getContext());
 					try {
-						db.addItem(inputName.getText().toString(), Double.valueOf(inputPrice.getText().toString()));
+						if (isWeightBox.isChecked()) {
+							db.addItem(inputName.getText().toString() + " (" + weightValue.getText().toString() + " lbs)", byWeightPrice);
+						} else {
+							db.addItem(inputName.getText().toString(), Double.valueOf(inputPrice.getText().toString()));
+						}
 					}
 					catch (SQLiteException e) {
 						Log.e(getString(R.string.app_name), "SQLite Error: " + e.getMessage());
@@ -81,6 +129,17 @@ public class ItemDetailsActivity extends AppCompatActivity {
 					startActivity(new Intent(view.getContext(), MainActivity.class));
 				}
 
+			}
+		});
+
+		isWeightBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+				if (b) {
+					weightLayout.setVisibility(View.VISIBLE);
+				} else {
+					weightLayout.setVisibility(View.INVISIBLE);
+				}
 			}
 		});
 
@@ -104,5 +163,22 @@ public class ItemDetailsActivity extends AppCompatActivity {
 				return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void updateByWeightPrice() {
+		if (inputPrice.getText().length() != 0 && weightValue.getText().length() != 0) {
+			Double pricePerPound = Double.valueOf(inputPrice.getText().toString());
+			Double pounds = Double.valueOf(weightValue.getText().toString());
+			Double finalPrice = pricePerPound * pounds;
+
+			finalPriceView.setText(String.format(Locale.getDefault(), "$%.2f", finalPrice));
+
+			byWeightPrice = finalPrice;
+			weightPriceSummary.setVisibility(View.VISIBLE);
+		}
+		else {
+			weightPriceSummary.setVisibility(View.INVISIBLE);
+		}
+
 	}
 }
